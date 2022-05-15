7eb92d7faa652926e5a0cad8d65eeaea24652881
bhavyatyagi <bhavyatyagi16@gmail.com>
2022-05-16 00:44:46 +0530

    add new transitions

--- script.js
+++ script.js
@@ -144,7 +144,7 @@ window.addEventListener('scroll', function () {
 
 const faders = document.querySelectorAll('.fade-in');
 const sliders = document.querySelectorAll('.slide-in');
-const appearOptions = { threshold: 0, rootMargin: "0px 0px -250px 0px" };
+const appearOptions = { threshold: 0, rootMargin: "0px 0px -325px 0px" };
 const appearOnScroll = new IntersectionObserver(
   function (enteries, appearOnScroll) {
     enteries.forEach(entry => {
