52ff8687ce536bec147358a60e0e8aedb922ffd0
bhavyatyagi <bhavyatyagi16@gmail.com>
2022-05-16 00:37:51 +0530

    add new transitions

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Loader jQuery
-$(window).on("load",function(){
+$(window).on("load", function () {
   $(".overlay-loader").fadeOut("slow");
 });
 
@@ -74,76 +74,93 @@ $(window).on("load",function(){
 
 //Skill Section Animation Bars IMPROVED
 
-var progressBars=document.querySelectorAll(".skills-progress > div");
+var progressBars = document.querySelectorAll(".skills-progress > div");
 
-var visited=false;
-function initialiseBar(bar)
-{
-  bar.setAttribute("data-visited",false);
-  bar.style.width=0+'%';
+var visited = false;
+function initialiseBar(bar) {
+  bar.setAttribute("data-visited", false);
+  bar.style.width = 0 + '%';
 }
-for(var bar of progressBars)
-{
+for (var bar of progressBars) {
   initialiseBar(bar);
 }
-function fillBar(bar){
-  var currentWidth=0;
-  var targetwidth=bar.getAttribute("data-bar-width");
-  var interval=setInterval(function(){
-    if(currentWidth>=targetwidth)
-    {
+function fillBar(bar) {
+  var currentWidth = 0;
+  var targetwidth = bar.getAttribute("data-bar-width");
+  var interval = setInterval(function () {
+    if (currentWidth >= targetwidth) {
       clearInterval(interval);
       return;
     }
     currentWidth++;
-    bar.style.width=currentWidth+'%';
-  },10);
+    bar.style.width = currentWidth + '%';
+  }, 10);
 }
-function improvedScroll(){
-  for(let bar of progressBars)
-  {
-    var barCoordinates=bar.getBoundingClientRect();
-    if((bar.getAttribute("data-visited")=="false")&&(barCoordinates.top<=(window.innerHeight-barCoordinates.height)))
-    {
-      bar.setAttribute("data-visited",true);
+function improvedScroll() {
+  for (let bar of progressBars) {
+    var barCoordinates = bar.getBoundingClientRect();
+    if ((bar.getAttribute("data-visited") == "false") && (barCoordinates.top <= (window.innerHeight - barCoordinates.height))) {
+      bar.setAttribute("data-visited", true);
       fillBar(bar);
     }
-    else if(barCoordinates.top>window.innerHeight)
-    {
-      bar.setAttribute("data-visited",false);
+    else if (barCoordinates.top > window.innerHeight) {
+      bar.setAttribute("data-visited", false);
       initialiseBar(bar);
     }
   }
 }
 
-window.addEventListener("scroll",improvedScroll);
-window.addEventListener("load",improvedScroll);
+window.addEventListener("scroll", improvedScroll);
+window.addEventListener("load", improvedScroll);
 
 
 
 //Contact-Form Submission Alert
-$(".contact-form button").click(function(event){
-  if($("#required_stuff").val()==="" || $("#required_name").val()==="")
-  alert("Please fill the required details properly!");
+$(".contact-form button").click(function (event) {
+  if ($("#required_stuff").val() === "" || $("#required_name").val() === "")
+    alert("Please fill the required details properly!");
   else
-  alert("Thank you for your time. Mr. Tyagi will get back to you soon!");
+    alert("Thank you for your time. Mr. Tyagi will get back to you soon!");
 });
 
 
 // Site Progress bar
-var filler=document.getElementById('filler');
-function getDocHeight(){
-    var d=document;
-    return Math.max(d.body.offsetHeight,d.body.scrollHeight,d.body.clientHeight);
+var filler = document.getElementById('filler');
+function getDocHeight() {
+  var d = document;
+  return Math.max(d.body.offsetHeight, d.body.scrollHeight, d.body.clientHeight);
 }
-var docHeight=getDocHeight();
-var windowHeight=window.innerHeight;
-window.addEventListener('resize',function(){
-   	docHeight=getDocHeight();
-    windowHeight=window.innerHeight;
+var docHeight = getDocHeight();
+var windowHeight = window.innerHeight;
+window.addEventListener('resize', function () {
+  docHeight = getDocHeight();
+  windowHeight = window.innerHeight;
 });
-window.addEventListener('scroll',function(){
-   	docHeight=getDocHeight();
-     var scrolled = Math.floor((window.scrollY/(docHeight-windowHeight))*100);
-    filler.style.width=scrolled+"%";
+window.addEventListener('scroll', function () {
+  docHeight = getDocHeight();
+  var scrolled = Math.floor((window.scrollY / (docHeight - windowHeight)) * 100);
+  filler.style.width = scrolled + "%";
+});
+
+const faders = document.querySelectorAll('.fade-in');
+const sliders = document.querySelectorAll('.slide-in');
+const appearOptions = { threshold: 0, rootMargin: "0px 0px -250px 0px" };
+const appearOnScroll = new IntersectionObserver(
+  function (enteries, appearOnScroll) {
+    enteries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add('appear');
+        appearOnScroll.unobserve(entry.target);
+      }
+    });
+  }, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
+
+sliders.forEach(slider => {
+  appearOnScroll.observe(slider);
 });
